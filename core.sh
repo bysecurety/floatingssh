5ac0a8c81bbe52c9ed8d03c8333dc4dd7e43162a
Securety <124460207+bysecurety@users.noreply.github.com>
2023-05-18 15:44:33 -0400

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-#Variables
+#VARIABLE DECLARATION
 host_ip="54.39.22.16"
 db_ip="54.39.22.16"
 db_name="floatingssh"
 password="wv2006"
 date=$(date -I)
 
-#sudo chmod 777 port.txt
 
-#Randomly generating a new SSH Port number, between 1024 and 32767
+#Randomly generating a new SSH Port number between 1024 and 32767
 new_port=$((1024 + $RANDOM % 32767))
 
-
-#Old/current port detection
+#Current Port Detection
 old_port=$(sudo netstat -tulpan | grep sshd | cut -d ":" -f 2 | cut -d " " -f 1 | head -1)
 
-
-#Checking if newly generated port number is already being used 
+#Checking if newly generated Port Number is already being used 
+#If this is positive a new Number will be generated and the old Port will be updated
 port_used=$(sudo netstat -tulpan | grep ":$new_port") 
 
 if [ "$port_used" == "" ];
@@ -26,28 +24,14 @@ then
 echo "Changing SSH Port number ..."
 sleep 1
 
+######################################UPDATING /ETC/SSH/SSHD_CONFIG#########################
 sudo sed -i "s/Port $old_port/Port $new_port/" /etc/ssh/sshd_config
-sudo sed -i "s/SSH Port Number $old_port/SSH Port Number $new_port/" /var/www/html/index.nginx-debian.html
 
-
-else
-
-echo "Port already used, electing new port"
-sleep 1
-
-new_port=$((1024 + $RANDOM % 32767))
-sed -i "s/Port $old_port/Port $new_port/" /etc/ssh/sshd_config
-sudo sed -i "s/SSH Port Number $old_port/SSH Port Number $new_port/" /var/www/html/index.nginx-debian.html
-
-
-fi
-
-################################################################################################
-#Parsing new SSH port to Mysql database
+############################################################################################
 #
-#Connecting to database
-
-
+#
+######################################MYSQL DB UPDATE#######################################
+#Parsing new SSH port to Mysql database that is configured in /etc/floatingssh/fssh.conf
 sudo mysql -u 'root' --password="$password" << EOF
 
 USE $db_name;
@@ -55,17 +39,41 @@ USE $db_name;
 DELETE FROM sshports WHERE host = '$host_ip';
 INSERT INTO sshports ( host, portnum, date) VALUES ( '$host_ip', '$new_port', '2023-05-18');
 
-select * from sshports;
+EOF
+############################################################################################
+
+else
+
+
+echo "Port already used, electing new port"
+sleep 1
+
+#########GENERATING NEW PORT NUMBER
+new_port=$((1024 + $RANDOM % 32767))
+
+###############UPDATING /ETC/SSH/SSHD_CONFIG
+sed -i "s/Port $old_port/Port $new_port/" /etc/ssh/sshd_config
+############################################################################################
+#
+#
+############MYSQL DB UPDATE
+#Parsing new SSH port to Mysql database that is configured in /etc/floatingssh/fssh.conf
+sudo mysql -u 'root' --password="$password" << EOF
+
+USE $db_name;
+
+DELETE FROM sshports WHERE host = '$host_ip';
+INSERT INTO sshports ( host, portnum, date) VALUES ( '$host_ip', '$new_port', '2023-05-18');
 
 EOF
+############################################################################################
 
+fi
 
-#Adding and deleting firewall rules
-
+#ADDING AND REMOVING FIREWALL RULES
 sudo ufw delete allow $old_port
 sudo ufw allow $new_port
 sudo ufw status | grep $new_port
 
-echo "New Port:"
-cat /etc/ssh/sshd_config | grep Port
-
+#RESTARTING SSHD TO APPLY NEW PORT CHANGE
+sudo systemctl restart sshd
