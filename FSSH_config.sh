5ccf36ae1156e612e6ff181e7d69b1e07075c899
Securety <124460207+bysecurety@users.noreply.github.com>
2023-05-18 22:05:45 -0400

    Update FSSH_config.sh

--- FSSH_config.sh
+++ FSSH_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-###########Floating SSH Configuration File
+#Floating SSH CONFIGURATION
 
 ####HOST SPECIFIC
 #Your server's IPv4 address
@@ -7,7 +7,7 @@ host_ip=""
 
 
 ####DATABASE SPECIFIC
-db_ip="54.39.22.16"     #Database IPv4 address
+db_ip=""     #Database IPv4 address
 db_name="floatingssh"   #Name of the database that will receive all the entries
 password=""       #Database user password
 db_user="FSSH"          #Database username
