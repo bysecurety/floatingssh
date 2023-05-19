ccb8d32a7d644d54dd3a13998dfda39796da65bd
Securety <124460207+bysecurety@users.noreply.github.com>
2023-05-18 21:53:01 -0400

    Add files via upload

---
+++ FSSH_config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+###########Floating SSH Configuration File
+
+####HOST SPECIFIC
+host_ip="54.39.22.16"
+
+
+####DATABASE SPECIFIC
+db_ip="54.39.22.16"
+db_name="floatingssh"
+password="wv2006"
+
+####MISC
+date=$(date -I)
+
+####LOGGING SPECIFIC
